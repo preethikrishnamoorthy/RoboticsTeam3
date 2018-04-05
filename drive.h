e9de7cff2960c05f2c33b5f79dc1c024576f5786
Preethi Krishnamoorthy <34527771+preethikrishnamoorthy@users.noreply.github.com>
2018-04-04 20:32:03 -0400

    Add files via upload

---
+++ drive.h
@@ -0,0 +1,29 @@
+void setArm(int speed) //raise or lower arm
+{
+	motor[armRight] = speed;
+	motor[armLeft] = speed;
+}
+
+void stopArm() //stop arm movement
+{
+	motor[armRight] = 0;
+	motor[armLeft] = 0;
+}
+
+void drive(int leftSpeed, int rightSpeed) //move robot
+{
+	motor[leftFront] = leftSpeed;
+	motor[leftBack] = leftSpeed;
+	motor[rightFront] = rightSpeed;
+	motor[rightBack] = rightSpeed;
+}
+
+void setClaw(int speed) //open or close claw
+{
+	motor[claw] = speed;
+}
+
+void stopClaw() //stop claw movement
+{
+	motor[claw] = 0;
+}
