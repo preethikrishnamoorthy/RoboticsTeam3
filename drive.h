b630ac7452581df6f4601a5ad1d9df024506860b
Preethi Krishnamoorthy <34527771+preethikrishnamoorthy@users.noreply.github.com>
2018-04-22 18:39:03 -0400

    add motors

--- drive.h
+++ drive.h
@@ -2,12 +2,16 @@ void setArm(int speed) //raise or lower arm
 {
 	motor[armRight] = speed;
 	motor[armLeft] = speed;
+	motor[armRightTop] = speed;
+	motor[armLeftTop] = speed;
 }
 
 void stopArm() //stop arm movement
 {
 	motor[armRight] = 0;
 	motor[armLeft] = 0;
+	motor[armRightTop] = 0;
+	motor[armLeftTop] = 0;
 }
 
 void drive(int leftSpeed, int rightSpeed) //move robot
